7341ea4308ac45e54adf00bd954a933115651d33
María Torrente <caritotorrente@gmail.com>
2020-11-02 19:08:36 -0500

    tests and snapshots

--- src/Components/List.test.js
+++ src/Components/List.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 import List from "./List";
 
 it("renders OK", () => {
   const div = document.createElement("div");
   ReactDOM.render(<List cards={[]} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders the UI as expected", () => {
+  const tree = renderer.create(<List cards={[]} />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
